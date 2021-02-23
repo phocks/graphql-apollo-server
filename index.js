632efba66a10fe80881f3a99c135c05dc8af9a14
Joshua Byrd <phocks@gmail.com>
2021-02-24 06:27:13 +1000

    change port

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen("65001").then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
